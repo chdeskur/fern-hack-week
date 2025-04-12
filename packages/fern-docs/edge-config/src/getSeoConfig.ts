6036a90f428004931b68ae40ff13d5180254f609
Deep Singhvi <deep@buildwithfern.com>
2025-04-12 15:36:30 -0400

    feat: introduce `pnpm docs:start:local` command to start a local development server (#2585)

--- packages/fern-docs/edge-config/src/getSeoConfig.ts
+++ packages/fern-docs/edge-config/src/getSeoConfig.ts
@@ -2,8 +2,6 @@ import { get } from "@vercel/edge-config";
 
 import { isCustomDomain } from "@fern-docs/utils";
 
-type CanonicalUrl = Record<string, string>;
-
 export async function getSeoDisabled(domain: string): Promise<boolean> {
   const isSeoEnabled = (await get<string[]>("seo-enabled")) ?? [];
   if (isSeoEnabled.includes(domain)) {
@@ -15,11 +13,4 @@ export async function getSeoDisabled(domain: string): Promise<boolean> {
   }
   const isDisabled = (await get<string[]>("seo-disabled")) ?? [];
   return isDisabled.includes(domain);
-}
-
-export async function getCanonicalUrl(
-  domain: string
-): Promise<string | undefined> {
-  const config = await get<CanonicalUrl>("canonical-host");
-  return config?.[domain];
 }
